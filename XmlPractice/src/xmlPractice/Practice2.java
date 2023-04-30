52cb39b10d7181c6cd0957c9d036871841ef9c35
DESKTOP-VUEA0AQ\kopo24 <lss2star@gmail.com>
2023-04-30 17:02:53 +0900

    변수 이름을 k21_붙여서 수정

--- XmlPractice/src/xmlPractice/Practice2.java
+++ XmlPractice/src/xmlPractice/Practice2.java
@@ -7,54 +7,54 @@ import org.json.simple.JSONObject;
 public class Practice2 {
 
 	public static void main(String[] args) {
-		JSONObject jsonObject = new JSONObject();
+		JSONObject k21_jsonObject = new JSONObject();
 		
-		JSONArray dataArray = new JSONArray();
+		JSONArray k21_dataArray = new JSONArray();
 		
-		JSONObject dataObject = new JSONObject();
+		JSONObject k21_dataObject = new JSONObject();
 		
-		dataObject.put("name", "나연");
-		dataObject.put("studentid", "209901");
-		JSONArray score = new JSONArray();
+		k21_dataObject.put("name", "나연");
+		k21_dataObject.put("studentid", "209901");
+		JSONArray k21_score = new JSONArray();
 		
-		score.add(90);
-		score.add(100);
-		score.add(95);
-		dataObject.put("score" ,score);
-		dataArray.add(dataObject);
+		k21_score.add(90);
+		k21_score.add(100);
+		k21_score.add(95);
+		k21_dataObject.put("score" ,k21_score);
+		k21_dataArray.add(k21_dataObject);
 		
-		dataArray.add(oneRec("정연", 209902, 100, 85, 75));
-		dataArray.add(oneRec("모모", 209903, 90, 75, 85));
-		dataArray.add(oneRec("사나", 209904, 90, 85, 75));
-		dataArray.add(oneRec("지효", 209905, 80, 75, 85));
-		dataArray.add(oneRec("미나", 209906, 90, 85, 55));
-		dataArray.add(oneRec("다현", 209907, 70, 75, 65));
-		dataArray.add(oneRec("채영", 209908, 100, 75, 95));
-		dataArray.add(oneRec("쯔위", 209909, 80, 85, 95));
+		k21_dataArray.add(oneRec("정연", 209902, 100, 85, 75));
+		k21_dataArray.add(oneRec("모모", 209903, 90, 75, 85));
+		k21_dataArray.add(oneRec("사나", 209904, 90, 85, 75));
+		k21_dataArray.add(oneRec("지효", 209905, 80, 75, 85));
+		k21_dataArray.add(oneRec("미나", 209906, 90, 85, 55));
+		k21_dataArray.add(oneRec("다현", 209907, 70, 75, 65));
+		k21_dataArray.add(oneRec("채영", 209908, 100, 75, 95));
+		k21_dataArray.add(oneRec("쯔위", 209909, 80, 85, 95));
 		
 		try {
-			FileWriter file= new FileWriter("C:\\Users\\kopo24\\Desktop\\scorejson.json");
-			file.write(dataArray.toJSONString());
-			file.flush();
-			file.close();
+			FileWriter k21_file= new FileWriter("C:\\Users\\kopo24\\Desktop\\scorejson.json");
+			k21_file.write(k21_dataArray.toJSONString());
+			k21_file.flush();
+			k21_file.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		System.out.println("JSON 만든거 : " + dataArray);
+		System.out.println("JSON 만든거 : " + k21_dataArray);
 
 	}
-	public static JSONObject oneRec(String name, int studentid, int kor, int eng, int mat) {
-		JSONObject dataObject = new JSONObject();
-		dataObject.put("name", name);
-		dataObject.put("studentid", studentid);
+	public static JSONObject oneRec(String k21_name, int k21_studentid, int k21_kor, int k21_eng, int k21_mat) {
+		JSONObject k21_dataObject = new JSONObject();
+		k21_dataObject.put("name", k21_name);
+		k21_dataObject.put("studentid", k21_studentid);
 		
-		JSONArray score = new JSONArray();
-		score.add(kor);
-		score.add(eng);
-		score.add(mat);
-		dataObject.put("score", score);
+		JSONArray k21_score = new JSONArray();
+		k21_score.add(k21_kor);
+		k21_score.add(k21_eng);
+		k21_score.add(k21_mat);
+		k21_dataObject.put("score", k21_score);
 		
-		return dataObject;
+		return k21_dataObject;
 	}
 
 }
